e8e6b0137b7adf88b631b4ac26c082c260249c15
Carlos Fung <carlos.fung@me.com>
2021-10-18 18:27:28 +0200

    conditionals

--- js/index.js
+++ js/index.js
@@ -1,15 +1,29 @@
 // Iteration 1: Names and Input
 // 
 
-let hacker1 = "Carlos";
+console.log("I'm ready!");
+
+var hacker1 = "Greggory";
 console.log(`The driver's name is ${hacker1}`);
 
-let hacker2 = "Greg";
+var hacker2 = "Greggory";
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+let driver = hacker1.length;
+let navigator = hacker2.length;
+
+if (driver > navigator ){
+    console.log(`The driver has the longest name, it has ${driver} characters. `);
+}
+else if (navigator > driver){
+    console.log(`It seems that the navigator has the longest name, it has ${navigator} characters. `);
+}
+else {
+    console.log(`Wow, you both have equally long names, ${driver} characters!.`);
+}
+
 
 // Iteration 3: Loops
 
-console.log("I'm ready!");
