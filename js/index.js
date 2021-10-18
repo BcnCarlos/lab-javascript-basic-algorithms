ff51dac15dbbaf924edef24a0dc304cde7311f6e
Carlos Fung <carlos.fung@me.com>
2021-10-18 22:58:45 +0200

    iteration 3

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // Iteration 1: Names and Input
-// 
+//  
 
 console.log("I'm ready!");
 
-var hacker1 = "Greggory";
+var hacker1 = "Carlos";
 console.log(`The driver's name is ${hacker1}`);
 
 var hacker2 = "Greggory";
@@ -27,3 +27,38 @@ else {
 
 // Iteration 3: Loops
 
+/* Print all the characters of the driver's name, separated by a space and in capitals  */
+
+var upperCase = "";
+for (var i=0; i<hacker1.length; i++){
+upperCase += hacker1.charAt(i).toUpperCase() + ' ';
+}
+console.log(upperCase);
+
+
+/* Print all the characters of the navigator's name */
+
+var newString = "";
+for (var i=hacker2.length -1; i >= 0; i-- ){
+newString += hacker2[i];
+
+}
+console.log(newString);
+
+/* Lexicographic order */
+
+function arrayOfString(str1, str2){
+
+
+if (str1 < str2) {
+    console.log("The driver's name goes first");
+} 
+else if (str2 < str1) {
+    console.log("Yo, the navigator goes first definitely.");
+}
+else {
+    console.log("What?! You both have the same name?");
+}
+}
+
+arrayOfString(hacker1, hacker2);
