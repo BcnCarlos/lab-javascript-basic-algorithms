b72c292c40a652c1f99f6c35748a89a0c7a48e15
Carlos Fung <carlos.fung@me.com>
2021-10-18 18:15:16 +0200

    hacker

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 // Iteration 1: Names and Input
 // 
 
+let hacker1 = "Carlos";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Greg";
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
 
